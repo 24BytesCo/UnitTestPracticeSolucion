e161f0bba981943e0f1f274182e2bf1b2324a5c5
Alexis Dorado Muñoz <alexis.dorado.munoz@gmail.com>
2023-10-19 00:25:17 -0500

    Se crea modelo y anotación personalizada para evaluar si la fecha es futura o no.

---
+++ Practice.Domain/Course.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Domain
+{
+    public class Course
+    {
+        [Key]
+        public Guid CourseId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Title { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string? Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [DateInFuture]
+        public DateTime PublicationDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreationDate { get; set; }
+
+        public Decimal Price { get; set; }
+
+    }
+}
---
+++ Practice.Domain/DateInFutureAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Practice.Domain
+{
+    // La clase es una anotación personalizada que se utiliza para validar si una fecha es futura o no.
+    public class DateInFutureAttribute : ValidationAttribute
+    {
+        private readonly Func<DateTime> _dateTimeReceived;
+
+        // Constructor predeterminado que utiliza DateTime.Now como fuente de tiempo para la validación.
+        public DateInFutureAttribute() : this(() => DateTime.Now)
+        {
+
+        }
+
+        // Constructor que permite proporcionar una fuente de tiempo personalizada para la validación.
+        public DateInFutureAttribute(Func<DateTime> dateTimeReceived)
+        {
+            _dateTimeReceived = dateTimeReceived;
+            ErrorMessage = "La fecha debe ser futura"; // Mensaje de error predeterminado si la fecha no es futura.
+        }
+
+        // Método IsValid es invocado durante la validación para determinar si la fecha es futura.
+        public override bool IsValid(object value)
+        {
+            bool isValid = false;
+
+            if (value is DateTime dateTime)
+            {
+                // Compara la fecha proporcionada con la fecha actual o la fecha personalizada y establece isValid en true si la fecha es futura.
+                isValid = dateTime > _dateTimeReceived();
+            }
+
+            return isValid; 
+        }
+    }
+}
