16cfd568e6893542aba651c16f515fd136ea0a29
Alexis Dorado Muñoz <alexis.dorado.munoz@gmail.com>
2023-10-19 13:48:00 -0500

    Se crea primer query para consulta de todas los cursos, se inicializa clase mapping

---
+++ Practice.Application/Feature/Courses/Queries/GetAllCoursesQueryHandled.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Practice.Domain;
+using Practice.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Application.Feature.Courses.Queries
+{
+    public class GetAllCoursesQueryHandled : IRequestHandler<GetAllCoursesQueryRequest, List<Course>>
+    {
+        private readonly PracticeDbContext _context;
+
+        private readonly IMediator _mediator;
+
+        public GetAllCoursesQueryHandled(PracticeDbContext context, IMediator mediator)
+        {
+            _context = context;
+            _mediator = mediator;
+        }
+
+
+
+        public async Task<List<Course>> Handle(GetAllCoursesQueryRequest request, CancellationToken cancellationToken)
+        {
+            return await _context.Courses.ToListAsync(cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ Practice.Application/Feature/Courses/Queries/GetAllCoursesQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Practice.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Application.Feature.Courses.Queries
+{
+    public class GetAllCoursesQueryRequest : IRequest<List<Course>>
+    {
+    }
+}
